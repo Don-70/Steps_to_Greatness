cc79b9ccf90034cd0e114338f67c325f45ce1712
Dudley R. Gamino <dudleyr.gamino@Dudleys-Air.attlocal.net>
2022-09-01 13:10:51 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+const app = express();
+// configre settings
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const DATABASE_URI = process.env.DATABASE_URI;
+const db = mongoose.connection;
+
+// Connect to Mongo DB
+
+mongoose.connect(DATABASE_URI);
+
+//add mongoDB
+db.on('connected', () => console.log(' Connected to Mongo DB'));
+db.on('error', (err) => console.log('Mongo ERROR: ' + err.message));
+
+// body parser middleware 
+
+app.use(express.urlencoded({ extented: false}));
+app.use(methodOverride('_method'));
+
+
+
+app.listen(PORT, () => {
+    console.log(`Express is listening on port${PORT}`);
+});
